047666cca1ca6da350b18eed8739a1396e3b3578
Gabriel Jardim <gabrielrlj99@outlook.com>
2022-01-23 13:51:23 -0300

    projeto criado

---
+++ src/main/java/com/jardim/apitmdb/ApiTmdbApplication.java
@@ -0,0 +1,13 @@
+package com.jardim.apitmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTmdbApplication.class, args);
+	}
+
+}
